a2e60e4ba2a4d9d1ed43f553a6654f1880bb838a
Abdera7mane <59313921+Abdera7mane@users.noreply.github.com>
2020-03-19 20:39:51 +0100

    Create example.gd

---
+++ GScripts/example.gd
@@ -0,0 +1,19 @@
+extends GameScript
+
+func _ready():
+	_handle_events(true)
+	Console.sendMessage("[ [color=lime]![/color] ] [b]Example Script is ready[/b]")
+
+# warning-ignore:unused_argument
+func _onCommand(event: GameEvent, command: String, args: Array):
+	if command == "hello":
+		if args[0] == "world":
+			Console.sendMessage("hell on you")
+			print(Console.console_text.get_line_count())
+			return
+	var expression: Expression = Expression.new()
+	var error = expression.parse(command, [])
+	if error != OK:
+		print(expression.get_error_text())
+		return
+	Console.sendMessage(str(expression.execute([command])))
