3e5e1c4209708e09f3d8b724125165df75c42dac
Abdera7mane <59313921+Abdera7mane@users.noreply.github.com>
2020-03-24 16:46:37 +0100

    Add files via upload

--- GameSctipt/GameScript.gd
+++ GameSctipt/GameScript.gd
@@ -1,24 +1,45 @@
+class_name GameScript
 extends Node
 
-class_name GameScript
+onready var EventHandler: Script = preload("../GameEvent/EventHandler.gd")
 
 #GameScript class properties
 var enabled: bool = false
+var config_file: ConfigFile setget set_configuration #, get_configuration
 
 #GameScript data information
-var script_name: String
 var version: String
-var authors: Array
+var authors: String
 var description: String
 
-static func get_script_config() -> ConfigFile:
-	var file: File = File.new()
-	if file.file_exists("./script.cfg"):
-		var config: ConfigFile = ConfigFile.new()
-		config.load("./script.cfg")
-		return config
-	return ConfigFile.new()
-	"""
-	return the GameScript config file 'script.cfg'
-	which usualy located in the same GameScript directory
-	"""
+# warning-ignore:shadowed_variable
+# warning-ignore:shadowed_variable
+# warning-ignore:shadowed_variable
+func _init(name: String = "none", version: String = "Unknown", authors: String = "Anonymous", description: String = "") -> void:
+	self.name = name
+	self.version = version
+	self.authors = authors
+	self.description = description
+
+func set_configuration(config: ConfigFile) -> void:
+	config_file = config
+
+#func get_configuration() -> ConfigFile:
+#	return config_file
+#	"""
+#	return the GameScript config file 'script.cfg'
+#	which usualy located in the same GameScript directory
+#	"""
+
+func _handle_events(enable: bool) -> void:
+	var methods: Dictionary = EventHandler.get_events()
+	if enable:
+		for event in methods:
+			var method: String = methods.get(event)
+			if has_method(method):
+				get_tree().call_deferred("connect", event, self, method)
+	else:
+		for event in methods:
+			var method: String = methods.get(event)
+			if has_method(method):
+				get_tree().disconnect(event, self, method)
